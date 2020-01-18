b3c48691e9ff5fa12552f5d267553001033b2582
SereogaUmca <sergheicucheryavii@gmail.com>
2020-01-18 14:08:30 +0200

    Initialize Controller

---
+++ Hybrid.ai.Geoposition.WebApi/Hybrid.ai.Geoposition.WebApi/Controllers/GeoInformationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Hybrid.ai.Geoposition.BLL.Handlers.Interfaces;
+using Hybrid.ai.Geoposition.Common.Models.ResponseModel;
+using Microsoft.AspNetCore.Mvc;
+using static Hybrid.ai.Geoposition.Common.Models.BaseModels.Response.AppResponse;
+
+namespace Hybrid.ai.Geoposition.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeoInformationController : BaseController
+    {
+        [HttpGet("GetGeoPosition")]
+        public async Task<Response<GeoPosition>> GetGeoPosition([FromServices]IInformation handler)
+        {
+
+            var getIp = GetIpAddress();
+            var getInformation = await handler.GetGeoPosition(getIp);
+            
+            return getInformation;
+        }
+    }
+}
